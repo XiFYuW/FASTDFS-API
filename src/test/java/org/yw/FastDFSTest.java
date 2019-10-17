f67369b7fd87cdbf651cb6c1f431f7c4cf50a4ad
XiFYuW <xif_yuw@163.com>
2019-10-17 10:00:59 +0800

    new

--- src/test/java/org/yw/FastDFSTest.java
+++ src/test/java/org/yw/FastDFSTest.java
@@ -1,9 +1,11 @@
 package org.yw;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.HashMap;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @author https://github.com/XiFYuW
@@ -12,13 +14,23 @@ import java.util.HashMap;
 public class FastDFSTest {
 
     public static void main(String[] args) {
-        FastDFSClient fastDFSClient = new FastDFSClient();
-        File file = new File("D:\\Compressed\\pic\\1561884770(1).jpg");
+        File file = new File("D:\\Compressed\\pic\\a02.jpg");
+        BigInteger bi = null;
         try {
-            FastDFSResponse fastDFSResponse = fastDFSClient.uploadTo(new FileInputStream(file),"D:\\Compressed\\pic\\1561884770(1).jpg", new HashMap<>());
-            System.err.println(fastDFSResponse);
-        } catch (FileNotFoundException e) {
+            System.out.println(DigestUtils.md5Hex(new FileInputStream("D:\\Compressed\\pic\\1561884770(1).jpg")));
+            byte[] buffer = new byte[8192];
+            int len = 0;
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            FileInputStream fis = new FileInputStream(file);
+            while ((len = fis.read(buffer)) != -1) {
+                md.update(buffer, 0, len);
+            }
+            fis.close();
+            byte[] b = md.digest();
+            bi = new BigInteger(1, b);
+        } catch (NoSuchAlgorithmException | IOException e) {
             e.printStackTrace();
         }
+        System.out.println(bi.toString(16));
     }
 }
